e06b4da8a89cbb693bd733240971798d954ef23b
Shinho Lee <cytron1225@naver.com>
2026-05-22 21:45:32 +0900

    커스텀 로그인 필터 구현

--- src/main/java/wwn/backend/config/SecurityConfig.java
+++ src/main/java/wwn/backend/config/SecurityConfig.java
@@ -1,17 +1,88 @@
 package wwn.backend.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import wwn.backend.filter.LoginFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    // 커스텀 필터 체인을 위한 AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager AuthenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
     // 비밀번호 단방향 암호화용 Bean 등록
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // 시큐리티 필터 체인
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, HttpServletResponse httpServletResponse) throws Exception {
+
+        // CSRF 보안 필터 disable
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+
+        // CORS 설정
+
+
+        // 기본 Form 기반 로그인 disable
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
+
+
+        // 기본 베이직 로그인 disable
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        // 인가
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll());
+
+        // 커스텀 필터 체인 설정
+        http
+                .addFilterBefore(new LoginFilter(AuthenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+
+
+        // 예외 처리
+        http
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED); // 401 응답
+                        })
+                        .accessDeniedHandler((request, response, authException) -> {
+                            response.sendError(HttpServletResponse.SC_FORBIDDEN); // 403 응답
+                        })
+                );
+
+        // 세션 필터 설정(stateless)
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        return http.build();
     }
 }
